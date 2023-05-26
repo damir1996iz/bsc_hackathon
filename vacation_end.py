680bbe4a91eaa17593c4bc681af16941b2fed7c8
Damir Gilmutdinov <dgilmutdinov@bscpraha.cz>
2023-05-26 20:28:55 +0400

    Замена текстовок и добавление кнопок

--- vacation_end.py
+++ vacation_end.py
@@ -1,14 +1,12 @@
 from telegram.ext import ContextTypes
 from telegram import Update
 
-
 VACATION_TASK = "https://cz-support.finshape.com/jira/browse/P20009352-969"
 
 
 async def show_jira_task(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text="Не забудьте списать время в задачу {task} по 8 часов в каждый рабочий день".format(
-            task=VACATION_TASK
-        )
+        text="Отлично! Твой отпуск оформлен.Не забудь списать время в задачу {task} по 8 часов в каждый рабочий день"
+        .format(task=VACATION_TASK)
     )
--- welcome.py
+++ welcome.py
@@ -3,22 +3,29 @@ from telegram import Update
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from confluence import get_username_by_tg, get_user_vacations
 
-
 async def vacation_button_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
     """Обработка /vacation"""
 
-    message = "Вы выбрали функционал согласования отпусков"
+    message = "Выбери, какой отпуск оформляем (также ты можешь ознакомиться с <a " \
+              "href=\"https://cz-support.finshape.com/confl/x/na5eBQ\">регламентами</a> самостоятельно):"
 
-    keyboard = [[
-        InlineKeyboardButton(text="Оплачиваемый", callback_data="/next_vacation")
-    ]]
+    keyboard = [
+        [InlineKeyboardButton(text="Sick day", callback_data="/mock"),
+         InlineKeyboardButton(text="Оплачиваемый", callback_data="/next_vacation")],
+        [InlineKeyboardButton(text="За свой счет", callback_data="/mock"),
+         InlineKeyboardButton(text="Больничный", callback_data="/mock")],
+        [InlineKeyboardButton(text="Учебный", callback_data="/mock"),
+         InlineKeyboardButton(text="По беременности", callback_data="/mock")],
+        [InlineKeyboardButton(text="Уход за ребенком", callback_data="/mock")]
+    ]
 
     markup = InlineKeyboardMarkup(keyboard)
 
     await context.bot.send_message(
         chat_id=update.effective_chat.id,
         text=message,
-        reply_markup=markup
+        reply_markup=markup,
+        parse_mode="html"
     )
 
 
@@ -44,7 +51,7 @@ async def next_vacation_button_handler(update: Update, context: ContextTypes.DEF
     near_vacation = sorted(vacation_list, key=lambda item: item.start_date)[0]
     context.chat_data["next_vacation"] = near_vacation
 
-    message = "Ближайший отпуск запланирован с {} по {}".format(
+    message = "У тебя запланирован отпуск с {} по {}. Оформляем?".format(
         near_vacation.start_date.strftime("%d-%m-%Y"),
         near_vacation.end_date.strftime("%d-%m-%Y")
     )
