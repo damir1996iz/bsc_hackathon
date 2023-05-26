fe9c93e85fa44a771141cede76cf540167a5fc9e
Damir Gilmutdinov <dgilmutdinov@bscpraha.cz>
2023-05-26 11:02:51 +0400

    Никнейм из конфлюенса

---
+++ confluence.py
@@ -0,0 +1,27 @@
+from atlassian import Confluence
+from bs4 import BeautifulSoup
+
+
+def get_username_by_tg(tg: str):
+    confluence = Confluence(
+        url='https://cz-support.finshape.com/confl',
+        username='dgilmutdinov',
+        password='Damta96A'
+    )
+
+    page = confluence.get_page_by_id("169810267", "space,body.view,version,container")
+
+    data = {}
+
+    soup = BeautifulSoup(page["body"]["view"]["value"], features="html.parser")
+    table = soup.find("div", {"class": "table-wrap"})
+    rows = table.find_all("tr")[3:]
+    for r in rows:
+        columns = r.find_all("td")
+        name = columns[1].find("h4").text
+        user_tg = columns[3].find("div", {"class": "content-wrapper"}).find_all("p")[1].text
+        data[name] = user_tg
+
+    for key, value in data.items():
+        if value == tg:
+            return key
